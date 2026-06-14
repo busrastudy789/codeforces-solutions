844ddc66aa25afc20bfc353d12d5ac707e81cbb2
Busra Tabassum <busrastudy789@gmail.com>
2026-06-14 23:45:14 +0600

    Add files via upload

---
+++ Football.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int cnt = 1;
+
+    for (int i = 1; i < s.size(); i++) {
+        if (s[i] == s[i - 1]) {
+            cnt++;
+            if (cnt >= 7) {
+                cout << "YES\n";
+                return 0;
+            }
+        } else {
+            cnt = 1;
+        }
+    }
+
+    cout << "NO\n";
+    return 0;
+}
